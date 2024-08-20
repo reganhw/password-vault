22b3f439bd0997a6a9fddd131b178a2b7586ece9
RH <reganhwang@protonmail.com>
2024-08-20 02:16:45 +0100

    Connected MongoDB.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const errorHandler = require("./errorHandler");
+const connectDb = require("./connectDb");
+
+connectDb();
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 3000;
 
